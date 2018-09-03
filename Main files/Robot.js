42d6d591e462ca443c2d330d385accd261b41048
Azael del Rosario <azaeldrm@gmail.com>
2018-09-03 19:38:42 -0400

    Created updateXYZ function. Tested, it works.

--- Main files/Robot.js
+++ Main files/Robot.js
@@ -40,9 +40,25 @@ class Robot {
   updateFwKin() {
     this.armTMs[0] = this.arms[0].TM;
     for (var i = 1; i < this.n_arms; i++) {
-      this.armTMs[i] = math.multiply(this.armTMs[i-1],this.arms[i].TM)
+      this.armTMs[i] = math.multiply(this.armTMs[i-1],this.arms[i].TM);
     }
     console.log(this.armTMs);
+  }
+
+  updateXYZ() {
+    for (var i = 0; i < this.n_arms; i++) {
+      if (i == 0) {
+        this.arms[i].coordinates.x = [0, this.armTMs[i][0][3]];
+        this.arms[i].coordinates.y = [0, this.armTMs[i][1][3]];
+        this.arms[i].coordinates.z = [0, this.armTMs[i][2][3]];
+      }
+      else {
+        this.arms[i].coordinates.x = [this.armTMs[i-1][0][3], this.armTMs[i][0][3]];
+        this.arms[i].coordinates.y = [this.armTMs[i-1][1][3], this.armTMs[i][1][3]];
+        this.arms[i].coordinates.z = [this.armTMs[i-1][2][3], this.armTMs[i][2][3]];
+      }
+    }
+    console.log(this.arms)
   }
 
   // createIM() {
